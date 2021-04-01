da4d4e6ba7bbdcdbb313833c31a707b59e2faf3e
ChilpaAjay1990 <77816633+ChilpaAjay1990@users.noreply.github.com>
2021-03-31 20:56:56 -0500

    auqa

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aquaRouter = require('./routes/aqua');
+var starsRouter = require('./routes/stars');
+
 
 var app = express();
 
@@ -23,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/aqua', aquaRouter);
+app.use('/stars', starsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
