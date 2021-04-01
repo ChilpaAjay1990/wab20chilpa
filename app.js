b329aa09d8c19a55b2c13bd7f8be070631d0c5f7
ChilpaAjay1990 <77816633+ChilpaAjay1990@users.noreply.github.com>
2021-03-31 21:14:44 -0500

    added slot

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aquaRouter = require('./routes/aqua');
 var starsRouter = require('./routes/stars');
-
+var slotRouter = require('./routes/slot');
 
 var app = express();
 
@@ -26,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/aqua', aquaRouter);
 app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
